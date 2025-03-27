048898dc25cd885d384ea19ea559c9f442e0f1c7
Stefan Hahnl <hahnl.stefan@gmail.com>
2025-03-27 20:54:05 +0100

    feat: add backend service functions

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+import { RelocationRequestBackendService } from './shared/relocation-request-backend.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes), 
-    provideAnimationsAsync()
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    RelocationRequestBackendService,
   ]
 };
--- src/app/shared/relocation-request-backend.service.ts
+++ src/app/shared/relocation-request-backend.service.ts
@@ -1,8 +1,29 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RelocationRequest } from './interfaces/relocation-request';
+import { catchError, map, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RelocationRequestBackendService {
 
+  private apiUrl: string = "http://localhost:8090/request";
+
+  constructor(private http: HttpClient) { }
+
+  public sendRelocationRequest(data: Partial<RelocationRequest>): Observable<boolean> {
+    const url = `${this.apiUrl}`;
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+    console.log(data);
+
+    return this.http.post<RelocationRequest>(url, data, { headers }).pipe(
+      map(() => true),
+      catchError((error) => {
+        console.error('Error:', error);
+        return of(false);
+      })
+    );
+  }
 }
