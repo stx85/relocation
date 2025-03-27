68f462c817139188e4287335afc87e78deda35d7
stx85 <2310859015@hochschule-burgenland.at>
2025-03-27 19:52:49 +0100

    feat: add header content and necessary addons

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'relocation' title`, () => {
+  it(`should have the 'Relocation Request' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('relocation');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, relocation');
+    expect(app.title).toEqual('Relocation Request');
   });
 });
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  public title: string = 'Relocation';
+  public imagePath: string = "./../assets/images/relocation.jpg";
 }
