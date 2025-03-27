2f8ad6b9787cebfb30d90c19c0ce48ee87a4699c
stx85 <2310859015@hochschule-burgenland.at>
2025-03-27 19:06:41 +0100

    feat: add unit test for header

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
@@ -19,5 +18,19 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render image with given imagePath', () => {
+    fixture = TestBed.createComponent(HeaderComponent);
+    const img = fixture.nativeElement.querySelector('img') as HTMLImageElement;
+    fixture.detectChanges();
+    expect(img.src).toContain('assets/images/relocation.jpg');
+  });
+
+  it('should have title Relocation', () => {
+    fixture = TestBed.createComponent(HeaderComponent);
+    const title = fixture.nativeElement.querySelector('[class="title"]') as HTMLElement;
+    fixture.detectChanges();
+    expect(title.textContent).toContain('Relocation');
   });
 });
